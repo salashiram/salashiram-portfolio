ccc56ffc90834ddb5b9a0f1e462b420b93dfaf31
salashiram <hiramsalas@192.168.1.14>
2023-05-01 12:59:49 -0600

    footer

--- navBar.js
+++ navBar.js
@@ -31,4 +31,40 @@ windowTop.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
+const navbar = document.querySelector("#navbar"); // selecciona tu barra de navegación por su id
+let prevScrollpos = window.pageYOffset; // inicializa el previo scroll position
+
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.pageYOffset; // obtiene el scroll position actual
+  if (prevScrollpos > currentScrollPos) {
+    // si el usuario está desplazando hacia arriba
+    navbar.classList.remove("hidden"); // muestra la barra de navegación
+  } else {
+    // si el usuario está desplazando hacia abajo
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+      navbar.classList.add("hidden"); // oculta la barra de navegación si ha llegado al final de la página web
+    }
+  }
+  prevScrollpos = currentScrollPos; // actualiza el previo scroll position
+});
+
+// window.addEventListener("scroll", function () {
+//   if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+//     // El usuario ha llegado al final de la página web
+
+//     this.alert("has llegado al final");
+//   }
+// });
+
+const btnScrollToTop = document.querySelector(".scroll-to-top");
+
+// Add a click event listener to the button
+btnScrollToTop.addEventListener("click", () => {
+  // Scroll the page to the top with a smooth animation
+  window.scrollTo({
+    top: 0,
+    // behavior: "smooth", // Ya esta esta propiedad en el CSS
+  });
+});
+
 navSlide();
